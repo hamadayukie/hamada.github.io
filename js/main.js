a321f651a574b7890ba2f5c846989f6c31238b78
hamadayukie <90906813+hamadayukie@users.noreply.github.com>
2021-10-25 21:40:15 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
 {
-  const dts = document.querySelectorAll('dt');
+ const open = document.getElementById('open');
+ const overlay = document.querySelector('.overlay');
+ const close =document.getElementById('close');
 
-  dts.forEach(dt => {
-    dt.addEventListener('click',() => {
-      dt.parentNode.classList.toggle('appear');
+ open.addEventListener('click', () => {
+  overlay.classList.add('show');
+  open.classList.add('hide');
+ });
 
-      dts.forEach(el => {
-        if (dt !== el) {
-          el.parentNode.classList.remove('appear');
-        }
-      });
-    });
-  });
+ close.addEventListener('click', () => {
+  overlay.classList.remove('show');
+  open.classList.remove('hide');
+ });
 }
