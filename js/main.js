edc7fd04a7d34e650964cd87626a46485733e3dd
hamadayukie <90906813+hamadayukie@users.noreply.github.com>
2021-11-22 01:51:01 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-'use strict';
+function showElementAnimation() {
 
-{
- const open = document.getElementById('open');
- const overlay = document.querySelector('.overlay');
- const close =document.getElementById('close');
-
- open.addEventListener('click', () => {
-  overlay.classList.add('show');
-  open.classList.add('hide');
- });
-
- close.addEventListener('click', () => {
-  overlay.classList.remove('show');
-  open.classList.remove('hide');
- });
+  var element = document.getElementsByClassName('js-animation');
+  if(!element) return; // 要素がなかったら処理をキャンセル
+  
+  var showTiming = window.innerHeight > 768 ? 200 : 40; // 要素が出てくるタイミングはここで調整
+  var scrollY = window.pageYOffset;
+  var windowH = window.innerHeight;
+  
+  for(var i=0;i<element.length;i++) { var elemClientRect = element[i].getBoundingClientRect(); var elemY = scrollY + elemClientRect.top; if(scrollY + windowH - showTiming > elemY) {
+    element[i].classList.add('is-show');
+  } else if(scrollY + windowH < elemY) {
+    // 上にスクロールして再度非表示にする場合はこちらを記述
+    element[i].classList.remove('is-show');
+  }
 }
+}
+showElementAnimation();
+window.addEventListener('scroll', showElementAnimation);
